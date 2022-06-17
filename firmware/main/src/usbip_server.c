772727db011d5d82497236ad6175d82d879e586d
Jviraj <virajjagadale123@gmail.com>
2022-06-17 12:24:11 +0530

    Added semaphore handle to the 2 tasks

--- firmware/main/src/usbip_server.c
+++ firmware/main/src/usbip_server.c
@@ -13,11 +13,11 @@ TaskHandle_t *tcp_server_task = NULL;
 /* Initialises the server for USB IP */
 esp_err_t usbip_server_init()
 {
-    tcp_server_init();
+    SemaphoreHandle_t signaling_sem = xSemaphoreCreateBinary();
 
     // Starts reading the USB Devices
-    xTaskCreatePinnedToCore(usb_host_lib_daemon_task, "daemon usb task", 4096, NULL, 2, usb_daemon_task_hdl, 0);
-    xTaskCreatePinnedToCore(usb_class_driver_task, "class", 4096, NULL, 3, usb_class_driver_task_hdl, 0);
+    xTaskCreatePinnedToCore(usb_host_lib_daemon_task, "daemon usb task", 4096, (void *)signaling_sem, 2, usb_daemon_task_hdl, 0);
+    xTaskCreatePinnedToCore(usb_class_driver_task, "class", 4096, (void *)signaling_sem, 3, usb_class_driver_task_hdl, 0);
 
     ESP_LOGI(TAG, "Initialised the USB/IP server successfully");
     return ESP_OK;
